2eac15eb7bd8d60bbdd638bb1274758955b3c109
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-17 21:01:30 +0200

    NavBar implementation

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
